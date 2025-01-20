44466232704d82716903170cc241b816e081125a
iluzin <isluzin@gmail.com>
2025-01-20 03:41:25 +0300

    Update primes.py

--- primes.py
+++ primes.py
@@ -12,6 +12,35 @@ def eratosthenes_sieve(limit):
     primes = [2] + list(filter(None, primes))
     return primes
 
+def is_prime(n):
+    if n < 2:
+        return False
+    elif n <= 3:
+        return True
+    elif 1 != n % 6 != 5:
+        return False
+    for i in range(5, round(pow(n, .5)) + 1, 6):
+        if not (n % i and n % (i + 2)):
+            return False
+    return True
+
+def find_goldbach(n, primes=None):
+    if primes is None:
+        primes = eratosthenes_sieve(n >> 1)
+    for i in primes:
+        if is_prime(n - i):
+            return (i, n - i)
+    return None
+
+def find_all_goldbach_pairs(n, primes=None):
+    if primes is None:
+        primes = eratosthenes_sieve(n >> 1)
+    pairs = []
+    for i in primes:
+        if is_prime(n - i):
+            pairs.append((i, n - i))
+    return pairs
+
 def main(*args, **kwargs):
     primes = eratosthenes_sieve(1000000)
     print(len(primes))
